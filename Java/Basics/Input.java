c1e2a14c7116ec65587005f560ab7f4283adfd03
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 19:51:22 +0530

    How Java takes Input

---
+++ Java/Basics/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        int x;
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        s.close();
+        System.out.println(n);
+    }
+}
