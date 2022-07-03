b1b465a56ab93c1f97abc3ff23d657a009d1c43f
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 12:52:47 +0530

    1D Array Basics

---
+++ Java/Basics/1D Array.java
@@ -0,0 +1,7 @@
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        int arr[]={10,20,30,40};
+        System.out.println(arr.length);
+    }
+}
