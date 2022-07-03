bf2730d7e295ae2912249480acf34644547e97d4
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 12:45:43 +0530

    value of datatypes

---
+++ Java/Basics/dataTypes.java
@@ -0,0 +1,14 @@
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        byte t=1;
+        short b=3;//-127 to 127
+        int c=5;
+        long d=7;
+        boolean e=true;
+        char f='a';
+        float g=9.4f;
+        double h=9.40;
+        System.out.println( t+":"+b+":"+c+":"+d+":"+e+":"+f+":"+g+":"+h);
+    }
+}
