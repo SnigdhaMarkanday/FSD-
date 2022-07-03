86110583b84c53d93e291cd4c94cb114b80a5a6d
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 12:32:07 +0530

    Overflowing of Variable

---
+++ Java/Basics/Overflowing Variable.java
@@ -0,0 +1,9 @@
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        int overflowVariable = Integer.MAX_VALUE;
+        System.out.println(overflowVariable);
+        overflowVariable+=4;
+        System.out.println(overflowVariable);
+    }
+}
