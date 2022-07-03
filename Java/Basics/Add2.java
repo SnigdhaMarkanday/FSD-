e22241aa8cdae693872dc38fadcca3f43aff42e0
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 12:15:49 +0530

    Adding 2 no.s

---
+++ Java/Basics/Add2.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world ");
+        int n1=400;
+        int n2=8;
+        int s;
+        s=n1+n2;
+        System.out.println("Sum is "+s);
+    }
+}
