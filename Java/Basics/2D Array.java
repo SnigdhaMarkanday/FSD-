dde961a8776dc18060c2cb9fc83a64770124371e
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 14:55:35 +0530

    2D Array

---
+++ Java/Basics/2D Array.java
@@ -0,0 +1,8 @@
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        int arr[][]={{10,20,30,40},{50,60,70}};
+        int x=arr[1][2];
+        System.out.println(x);
+    }
+}
