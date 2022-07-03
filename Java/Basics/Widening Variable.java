65e3b7eae3e03b9897d28a1924541085ebcc224c
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-03 12:23:09 +0530

    Widening Variable

---
+++ Java/Basics/Widening Variable.java
@@ -0,0 +1,8 @@
+class Java {
+    public static void main(String[] args) {
+        System.out.println("This is Snigdha Hello world\n ");
+        int n1=400;
+        double wideningVariable = n1;
+        System.out.println("Widening Variable "+wideningVariable);
+    }
+}
