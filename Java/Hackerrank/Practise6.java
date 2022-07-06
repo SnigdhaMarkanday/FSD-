7b057f069262c31d71231b1b3aad70f86700ed56
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-07-06 14:03:20 +0530

    Loops1

---
+++ Java/Hackerrank/Practise6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int a1 = scan.nextInt();
+        int a2 = scan.nextInt();
+        scan.close();
+        
+        
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(a2);
+        // Complete this line
+        // Complete this line
+    }
+}
